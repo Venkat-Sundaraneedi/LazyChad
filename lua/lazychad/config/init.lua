2637d09e5c994237d362c917cc2386230f026f1b
Automated Commit <auto@commit.local>
2025-03-12 18:35:49 +0530

    Automated commit (2025-03-12 18:35:49) (LazyChad)

--- lua/lazychad/config/init.lua
+++ lua/lazychad/config/init.lua
@@ -10,9 +10,9 @@ LazyChad.config = M
 local defaults = {
   -- colorscheme can be a string like `catppuccin` or a function that will load the colorscheme
   ---@type string|fun()
-  colorscheme = function()
-    require("tokyonight").load()
-  end,
+  -- colorscheme = function()
+  --   require("tokyonight").load()
+  -- end,
   -- load the default settings
   defaults = {
     autocmds = true, -- lazychad.config.autocmds
