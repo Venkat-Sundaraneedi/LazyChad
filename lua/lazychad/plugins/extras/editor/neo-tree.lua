7ba72c9e0d6ec2d60605acabcb3ad75deb7f557d
Automated Commit <auto@commit.local>
2025-03-13 00:27:16 +0530

    Automated commit (2025-03-13 00:27:16) (LazyChad)

--- lua/lazychad/plugins/extras/editor/neo-tree.lua
+++ lua/lazychad/plugins/extras/editor/neo-tree.lua
@@ -68,7 +68,7 @@ return {
         position = "right", -- left, right, top, bottom, float, current
         width = 40, -- applies to left and right positions
         height = 15, -- applies to top and bottom positions
-        auto_expand_width = true, -- expand the window when file exceeds the window width. does not work with position = "float"
+        auto_expand_width = false, -- expand the window when file exceeds the window width. does not work with position = "float"
         popup = { -- settings that apply to float position only
           size = {
             height = "80%",
