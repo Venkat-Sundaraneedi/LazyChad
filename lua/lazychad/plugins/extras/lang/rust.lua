443073644dfe282aceba9bbbe94e302f2b7ef6bb
Automated Commit <auto@commit.local>
2025-03-13 13:51:24 +0530

    Automated commit (2025-03-13 13:51:24) (LazyChad)

--- lua/lazychad/plugins/extras/lang/rust.lua
+++ lua/lazychad/plugins/extras/lang/rust.lua
@@ -72,7 +72,7 @@ return {
           -- rust-analyzer language server configuration
           ["rust-analyzer"] = {
             rustfmt = {
-              extraArgs = { "--config", "tab_spaces=2,indent_style=block,indent_width=2" },
+              extraArgs = { "--config", "tab_spaces=4,indent_style=block,indent_width=2" },
             },
             cargo = {
               allFeatures = true,
